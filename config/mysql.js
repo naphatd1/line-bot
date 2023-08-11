53ca1a2c7f21b9f58a102ba4a6e729834e8c9323
Naphat <naphat.d@gmail.com>
2023-08-11 08:59:07 +0700

    first line 1

--- config/mysql.js
+++ config/mysql.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise')
 
 exports.connectMyql = async () => {
   const conn = await mysql.createConnection({
-    host: 'localhost',
+    host: process.env.DB_URL,
     user: 'root',
     password: '1234',
     database: 'expressdb',
